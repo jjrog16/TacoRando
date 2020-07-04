0f70f6f25ea59764e7ff22529d1c07bc74df755e
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-07-04 10:39:18 -0400

    Updated DatabaseEntities.kt to mimic TacoItem and will be used to store TacoItem information

--- app/src/main/java/com/example/android/tacorando/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/tacorando/database/DatabaseEntities.kt
@@ -1,6 +1,26 @@
 package com.example.android.tacorando.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.tacorando.network.TacoItem
+
 @Entity
 data class DatabaseEntities constructor(
-
+    @PrimaryKey
+    val url: String,
+    val slug: String?,
+    val name: String?,
+    val recipe: String?
 )
+
+// Take what is in the database and make that the TacoItem
+fun List<DatabaseEntities>.asDomainModel(): List<TacoItem> {
+    return map {
+        TacoItem(
+            url = it.url,
+            slug = it.slug,
+            name = it.name,
+            recipe = it.recipe
+        )
+    }
+}
